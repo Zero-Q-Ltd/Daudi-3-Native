0dca5c5e85163d4f02d051c3ce1b7b089edeed91
johninvictus <invictusnitude@gmail.com>
2019-05-14 12:33:28 +0300

    migrated to use androidx dependencies

--- app/src/main/java/com/zeroq/daudi_3_native/MainActivity.kt
+++ app/src/main/java/com/zeroq/daudi_3_native/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.zeroq.daudi_3_native
 
-import android.support.v7.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
