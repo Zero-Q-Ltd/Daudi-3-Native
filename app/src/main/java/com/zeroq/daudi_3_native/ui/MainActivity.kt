9f4afbd9bb1e3ea23987afffe1fd41639d4d11b1
johninvictus <invictusnitude@gmail.com>
2019-05-14 16:41:23 +0300

    enhanced dagger

--- app/src/main/java/com/zeroq/daudi_3_native/ui/MainActivity.kt
+++ app/src/main/java/com/zeroq/daudi_3_native/ui/MainActivity.kt
@@ -3,12 +3,20 @@ package com.zeroq.daudi_3_native.ui
 
 import android.os.Bundle
 import com.zeroq.daudi_3_native.R
+import com.zeroq.daudi_3_native.utils.Info
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity() {
+
+    @Inject
+    lateinit var info: Info
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test.text = info.text
     }
 }
