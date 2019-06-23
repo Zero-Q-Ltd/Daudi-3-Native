ff07f4af1fbb2afee0b4a237633a038b6dca30e6
johninvictus <invictusnitude@gmail.com>
2019-06-23 23:25:11 +0300

    clean up and removing activity am not using

--- app/src/main/java/com/zeroq/daudi_3_native/di/modules/ActivityBuilder.kt
+++ app/src/main/java/com/zeroq/daudi_3_native/di/modules/ActivityBuilder.kt
@@ -3,7 +3,6 @@ package com.zeroq.daudi_3_native.di.modules
 import com.zeroq.daudi_3_native.di.fragment_modules.FragmentMainModules
 import com.zeroq.daudi_3_native.ui.MainActivity
 import com.zeroq.daudi_3_native.ui.activate.ActivateActivity
-import com.zeroq.daudi_3_native.ui.home.HomeActivity
 import com.zeroq.daudi_3_native.ui.login.LoginActivity
 import com.zeroq.daudi_3_native.ui.splash.SplashActivity
 import dagger.Module
@@ -22,9 +21,6 @@ abstract class ActivityBuilder {
 
     @ContributesAndroidInjector
     internal abstract fun bindLoginActivity(): LoginActivity
-
-    @ContributesAndroidInjector
-    internal abstract fun bindHomeActivity(): HomeActivity
 
     @ContributesAndroidInjector
     internal abstract fun bindActivateActivity(): ActivateActivity
