615e158a47f0836f638ba974bf23d3e6f6e8c97c
johninvictus <invictusnitude@gmail.com>
2019-06-23 22:10:16 +0300

    added image to toolbar

--- app/src/main/java/com/zeroq/daudi_3_native/ui/MainActivity.kt
+++ app/src/main/java/com/zeroq/daudi_3_native/ui/MainActivity.kt
@@ -4,15 +4,38 @@ import android.content.Context
 import android.os.Bundle
 import com.zeroq.daudi_3_native.R
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.ActionBar
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import com.google.firebase.auth.FirebaseAuth
 import com.zeroq.daudi_3_native.commons.BaseActivity
+import com.zeroq.daudi_3_native.utils.ImageUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar.toolbar
+import timber.log.Timber
+import javax.inject.Inject
 
 
 class MainActivity : BaseActivity() {
+
+
+    @Inject
+    lateinit var firebaseAuth: FirebaseAuth
+
+    @Inject
+    lateinit var imageUtil: ImageUtil
+
+
+    lateinit var actionBar: ActionBar
 
     companion object {
         fun startActivity(context: Context) {
@@ -40,12 +63,42 @@ class MainActivity : BaseActivity() {
 
     private fun setToolbar() {
         setSupportActionBar(toolbar)
-
         supportActionBar!!.title = "Morning"
+
+        actionBar = supportActionBar as ActionBar
+
+
+        Glide.with(this)
+            .asBitmap()
+            .load(firebaseAuth.currentUser?.photoUrl)
+            .apply(RequestOptions.circleCropTransform())
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    val dst = imageUtil.dpToPx(this@MainActivity, 28)
+                    val resizedBit: Bitmap = Bitmap.createScaledBitmap(resource, dst, dst, false)
+
+                    val d = RoundedBitmapDrawableFactory.create(resources, resizedBit)
+                    setLogo(d)
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {}
+
+                override fun onLoadFailed(errorDrawable: Drawable?) {
+                    super.onLoadFailed(errorDrawable)
+                    setLogo(getDrawable(R.drawable.ic_profile))
+                }
+            })
+
     }
 
     private fun setupBottomNavigationBar() {
         val navController = findNavController(this, R.id.mainNavFragment)
         bottom_nav.setupWithNavController(navController)
+    }
+
+    private fun setLogo(d: Drawable) {
+        actionBar.setLogo(d)
+        actionBar.setDisplayUseLogoEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
     }
 }
