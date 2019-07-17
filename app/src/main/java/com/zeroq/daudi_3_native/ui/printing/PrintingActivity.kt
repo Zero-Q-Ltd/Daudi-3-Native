fa70e05bc7f77ff6466d728b8f7cd25dec333036
johninvictus <invictusnitude@gmail.com>
2019-07-17 16:07:22 +0300

    Printing viewmodel, initial design, added to dagger graph

--- app/src/main/java/com/zeroq/daudi_3_native/ui/printing/PrintingActivity.kt
+++ app/src/main/java/com/zeroq/daudi_3_native/ui/printing/PrintingActivity.kt
@@ -6,8 +6,12 @@ import com.zeroq.daudi_3_native.commons.BaseActivity
 
 class PrintingActivity : BaseActivity() {
 
+    lateinit var printingViewModel: PrintingViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_printing)
+
+        printingViewModel = getViewModel(PrintingViewModel::class.java)
     }
 }
