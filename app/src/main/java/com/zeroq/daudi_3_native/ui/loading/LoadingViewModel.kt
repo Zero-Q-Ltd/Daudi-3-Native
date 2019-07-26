5334f2e1a116254dfc8ce9dfb0277a73a01c5ed2
johninvictus <invictusnitude@gmail.com>
2019-07-26 21:19:25 +0300

    expire logic connected to dialog and all is on

--- app/src/main/java/com/zeroq/daudi_3_native/ui/loading/LoadingViewModel.kt
+++ app/src/main/java/com/zeroq/daudi_3_native/ui/loading/LoadingViewModel.kt
@@ -1,8 +1,44 @@
 package com.zeroq.daudi_3_native.ui.loading
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.zeroq.daudi_3_native.data.models.TruckModel
+import com.zeroq.daudi_3_native.data.models.UserModel
+import com.zeroq.daudi_3_native.data.repository.AdminRepository
+import com.zeroq.daudi_3_native.data.repository.DepotRepository
+import com.zeroq.daudi_3_native.vo.CompletionLiveData
+import com.zeroq.daudi_3_native.vo.Resource
 import javax.inject.Inject
 
-class LoadingViewModel @Inject constructor() : ViewModel() {
+class LoadingViewModel @Inject constructor(
+    adminRepo: AdminRepository,
+    var depotRepository: DepotRepository,
+    var firebaseAuth: FirebaseAuth
+) : ViewModel() {
 
+    private var _user: LiveData<Resource<UserModel>> = MutableLiveData()
+    private val _userId = MutableLiveData<String>()
+
+    private val _depotId = MutableLiveData<String>()
+
+    init {
+        _user = Transformations.switchMap(_userId, adminRepo::getAdmin)
+
+        _userId.value = firebaseAuth.uid
+    }
+
+    fun getUser(): LiveData<Resource<UserModel>> {
+        return _user
+    }
+
+    fun setDepoId(depotid: String) {
+        if (depotid != _depotId.value) _depotId.value = depotid
+    }
+
+    fun updateExpire(idTruck: String, minutes: Long): CompletionLiveData {
+        return depotRepository.updateLoadingExpire(_depotId.value!!, idTruck, minutes)
+    }
 }
