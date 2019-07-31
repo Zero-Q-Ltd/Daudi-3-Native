7b1cf4d834971aaddadd19128cb1740379a4c871
johninvictus <invictusnitude@gmail.com>
2019-07-31 22:04:44 +0300

    created loading order activity

---
+++ app/src/main/java/com/zeroq/daudi_3_native/ui/loading_order/LoadingOrderActivity.kt
@@ -0,0 +1,13 @@
+package com.zeroq.daudi_3_native.ui.loading_order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zeroq.daudi_3_native.R
+
+class LoadingOrderActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading_order)
+    }
+}
