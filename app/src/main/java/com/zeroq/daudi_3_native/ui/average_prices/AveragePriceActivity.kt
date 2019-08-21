3ce900cc206c7aa5b018f4a9b7326a8e2341d0f7
johninvictus <invictusnitude@gmail.com>
2019-08-21 22:15:28 +0300

    initial creation of average activity, progress - tomorrow

---
+++ app/src/main/java/com/zeroq/daudi_3_native/ui/average_prices/AveragePriceActivity.kt
@@ -0,0 +1,13 @@
+package com.zeroq.daudi_3_native.ui.average_prices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zeroq.daudi_3_native.R
+
+class AveragePriceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_average_price)
+    }
+}
